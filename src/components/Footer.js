edecba9cf500e2cd6ff67ca5254bcb86f26d4117
Andy K <36778047+kreienkaa@users.noreply.github.com>
2019-07-02 12:18:07 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ export default function Footer() {
         })}
       </ul>
       <ul className="copyright">
-        <li>&copy; Highlights</li>
+        <li>&copy; Andy Kreienkamp</li>
         <li>
           Design: <a href="http://html5up.net">HTML5 UP</a>
         </li>
