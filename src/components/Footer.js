8c73df134583e472816ca16519686ffc4e75560d
Andy K <36778047+kreienkaa@users.noreply.github.com>
2019-07-02 12:18:42 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,9 +21,6 @@ export default function Footer() {
         <li>
           Design: <a href="http://html5up.net">HTML5 UP</a>
         </li>
-        <li>
-          Demo Images: <a href="http://unsplash.com">Unsplash</a>
-        </li>
       </ul>
     </footer>
   );
